96423151fa079ab4fdcf0b75db91f7ef4c7f0f12
redig <luizredig@gmail.com>
2024-11-19 20:15:58 -0300

    feat(global-config): dynamically update device count on add/remove operations

--- src/com/github/luizredig/design_pattern_exam/singleton/GlobalConfig.java
+++ src/com/github/luizredig/design_pattern_exam/singleton/GlobalConfig.java
@@ -1,43 +1,51 @@
 package com.github.luizredig.design_pattern_exam.singleton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GlobalConfig {
     private static volatile GlobalConfig instance;
 
     private GlobalConfig() {
+        this.devices = new ArrayList<>();
+        this.deviceCount = 0;
     }
 
     public static GlobalConfig getInstance() {
         if (instance == null) {
-            instance = new GlobalConfig();
+            synchronized (GlobalConfig.class) {
+                if (instance == null) {
+                    instance = new GlobalConfig();
+                }
+            }
         }
         return instance;
     }
 
-    private String designPattern;
-    private String professor;
-    private String student;
+    private List<Device> devices;
+    private int deviceCount;
 
-    public String getGlobalDesignPattern() {
-        return designPattern;
+    public void addDevice(Device device) {
+        devices.add(device);
+        deviceCount = devices.size();
+        System.out.println("Device added: " + device.getName());
     }
 
-    public void setGlobalDesignPattern(String pattern) {
-        this.designPattern = pattern;
+
+    public void removeDevice(Device device) {
+        if (devices.remove(device)) {
+            deviceCount = devices.size();
+            System.out.println("Device removed: " + device.getName());
+        } else {
+            System.out.println("Device not found: " + device.getName());
+        }
     }
 
-    public String getGlobalProfessor() {
-        return professor;
+    public int getConnectedDevicesCount() {
+        return deviceCount;
     }
 
-    public void setGlobalProfessor(String professor) {
-        this.professor = professor;
-    }
-
-    public String getGlobalStudent() {
-        return student;
-    }
-
-    public void setGlobalStudent(String student) {
-        this.student = student;
+    public List<Device> getDevices() {
+        return devices;
     }
 }
