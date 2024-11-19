45d19e363b6c8b8d3e6e79d15a8f90ab837072cd
redig <luizredig@gmail.com>
2024-11-19 20:16:13 -0300

    feat(main): display dynamic device count and updated device list

--- src/com/github/luizredig/design_pattern_exam/Main.java
+++ src/com/github/luizredig/design_pattern_exam/Main.java
@@ -1,18 +1,36 @@
 package com.github.luizredig.design_pattern_exam;
 
+import com.github.luizredig.design_pattern_exam.singleton.Device;
 import com.github.luizredig.design_pattern_exam.singleton.GlobalConfig;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Welcome to the IoT Device Management System!");
+
         GlobalConfig config = GlobalConfig.getInstance();
 
-        config.setGlobalDesignPattern("Singleton");
-        config.setGlobalProfessor("Escobar");
-        config.setGlobalStudent("Redig");
+        Device device1 = new Device("Temperature Sensor", true);
+        Device device2 = new Device("Smart Light", false);
+        Device device3 = new Device("Security Camera", true);
 
-        System.out.println(config.getGlobalDesignPattern());
-        System.out.println(config.getGlobalProfessor());
-        System.out.println(config.getGlobalStudent());
+        config.addDevice(device1);
+        config.addDevice(device2);
+        config.addDevice(device3);
+
+        System.out.println("Connected Devices:");
+        for (Device device : config.getDevices()) {
+            System.out.println(device);
+        }
+
+        System.out.println("Total connected devices (Global Count): " + config.getConnectedDevicesCount());
+
+        config.removeDevice(device2);
+
+        System.out.println("Updated Connected Devices:");
+        for (Device device : config.getDevices()) {
+            System.out.println(device);
+        }
+
+        System.out.println("Total connected devices (Global Count): " + config.getConnectedDevicesCount());
     }
 }
-
